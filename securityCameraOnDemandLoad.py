ef84e17cee23267cea1b0d0484dd71c2e2b5d7eb
Diogo Brito <diogomsb1@gmail.com>
2022-08-24 15:03:19 -0300

    unused artefacts deleteds. Waitress installed + requirements.txt with project depedencies

--- securityCameraOnDemandLoad.py
+++ securityCameraOnDemandLoad.py
@@ -11,7 +11,7 @@ app = Flask(__name__, template_folder='./templates')
 addresses = subprocess.check_output(['arp', '-a'])
 
 networkAdds = addresses.decode('windows-1252').splitlines()
-cameraIps = []
+cameraIps = ['0']
 for networkMapItem in networkAdds :
     if len(networkMapItem) > 0 and networkMapItem.split()[1] == '60-23-a4-b4-1b-8f':
         cameraIps.append(networkMapItem.split()[0])
@@ -54,70 +54,6 @@ def index():
 @app.route('/video_feed/<string:ip>')
 def video_feed(ip):
     return Response(stream_with_context(gen_frames(ip)), mimetype='multipart/x-mixed-replace; boundary=frame')
-    #return Response(gen_frames(index), mimetype='multipart/x-mixed-replace; boundary=frame')
         
 if __name__ == '__main__':
-    app.run(debug=True)
-
-
-
-
-
-
-
-def processCamera():
-    # Create an object to read
-    # from camera
-    video = cv2.VideoCapture('rtsp://admin:NUHIDF@192.168.15.9:554/H.264')
-
-    # We need to check if camera
-    # is opened previously or not
-    if not video.isOpened():
-        print("Error reading video file")
-
-    # We need to set resolutions.
-    # so, convert them from float to integer.
-    frame_width = int(video.get(3))
-    frame_height = int(video.get(4))
-
-    size = (frame_width, frame_height)
-
-    # Below VideoWriter object will create
-    # a frame of above defined The output
-    # is stored in 'output.avi' file.
-    result = cv2.VideoWriter('output.avi',
-                            cv2.VideoWriter_fourcc(*'MJPG'),
-                            10, size)
-
-    while True:
-        ret, frame = video.read()
-
-        if ret:
-
-            # Write the frame into the
-            # file 'output.avi'
-            result.write(frame)
-
-            # Display the frame
-            # saved in the file
-            cv2.imshow('Frame', frame)
-
-            # Press q on keyboard
-            # to stop the process
-            if cv2.waitKey(1) & 0xFF == ord('q'):
-                break
-
-        # Break the loop
-        else:
-            break
-
-    # When everything done, release
-    # the video capture and video
-    # write objects
-    video.release()
-    result.release()
-
-    # Closes all the frames
-    cv2.destroyAllWindows()
-
-    print("The video was successfully saved")
+    app.run(debug=False, host='0.0.0.0')
---
+++ wsgi.py
@@ -0,0 +1,5 @@
+from securityCameraOnDemandLoad import app
+from waitress import serve
+
+if __name__ == '__main__':
+   serve(app)
