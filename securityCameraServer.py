2c54e455553ee5d8e85f4edd3d182c3998bcffc8
Diogo Brito <diogomsb1@gmail.com>
2022-09-21 09:58:28 -0300

    multi-thread processing created to record camera videos and render videos in web app at same time

--- securityCameraServer.py
+++ securityCameraServer.py
@@ -1,5 +1,5 @@
 from flask_mongoengine import MongoEngine
-from flask import Flask, request, Response, render_template, stream_with_context, redirect, url_for
+from flask import Flask, request, Response, render_template, stream_with_context, redirect, url_for, session
 import cv2
 import logging
 import subprocess
@@ -9,7 +9,6 @@ import math
 from mongoMapper import Camera, User
 import string
 import secrets
-from flask import session
 import time
 from threading import Thread
 
@@ -49,8 +48,6 @@ app.config["MONGODB_SETTINGS"] = [
 db.init_app(app)
 #configurando mongo
 
-camera_ips = []
-
 def load_cameras():
     #resgatando todas as cameras do mongo
     cameras = Camera.objects().all()
@@ -73,33 +70,54 @@ def load_cameras():
     #salvando na variavel global os ips das cameras para conexao
     return cameras_map
 
-def gen_frames(ip, request_type):
-    gen_frames(ip, None, request_type)
 
-# generate frame by frame from camera
-def gen_frames(ip, mac_address, request_type): 
+def convert_1d_to_2d(l, cols):
+    return [l[i:i + cols] for i in range(0, len(l), cols)]
 
-    ip_regex_check_result = re.search("^((25[0-5]|(2[0-4]|1[0-9]|[1-9]|)[0-9])(\.(?!$)|$)){4}$", ip)
+def get_cameras_matrix_size(camera_ips):
+    cameras_quantity = len(camera_ips)
+    return math.ceil(math.sqrt(cameras_quantity))
     
+def resize_img_from_matriz_size(frame, camera_matrix_size):
+    width = int(frame.shape[1] / camera_matrix_size)
+    height = int(frame.shape[0] / camera_matrix_size)
+    dim = (width, height)
+    return cv2.resize(frame, dim, interpolation = cv2.INTER_AREA)
+
+def build_video_capture(ip):
+    ip_regex_check_result = re.search("^((25[0-5]|(2[0-4]|1[0-9]|[1-9]|)[0-9])(\.(?!$)|$)){4}$", ip)
+        
     if ip_regex_check_result:
         rtsp_connetion = 'rtsp://admin:NUHIDF@'+ip+':554/H.264'
     else:
         rtsp_connetion = int(ip)
-    
-    cap = cv2.VideoCapture(rtsp_connetion)
-    if request_type == 'store':
-        get_frames_to_store(cap, mac_address)
-    else:
-        get_frames_to_view(cap)
+    return cv2.VideoCapture(rtsp_connetion)
 
+def gen_frames_by_ip_to_view(cap, camera_matrix_size):
+    while True:
+        success, frame = cap.read()
+        if success:
+            try:
+                resized_frame = resize_img_from_matriz_size(frame, camera_matrix_size)
+                ret, buffer = cv2.imencode('.jpg', resized_frame)
+                resized_frame = buffer.tobytes()
+                yield (b'--frame\r\n'
+                    b'Content-Type: image/jpeg\r\n\r\n' + resized_frame + b'\r\n')
+            except Exception as e:
+                logging.exception(e)
+                print(e)
+                pass    
+        else:
+            pass
 
+# recording camera locally
 def get_frames_to_store(cap, mac_address):
     
     fourcc = cv2.VideoWriter_fourcc('X','V','I','D')
     frame_width = int(cap.get(3))
     frame_height = int(cap.get(4))
     current_seconds = time.time()
-    out = cv2.VideoWriter(mac_address + '_' + str(current_seconds) + '.avi', fourcc, 20, (frame_width,frame_height), True)
+    out = cv2.VideoWriter(mac_address + '_' + str(current_seconds) + '.avi', fourcc, 20, (frame_width, frame_height), True)
 
     while True:
         success, frame = cap.read()
@@ -117,41 +135,28 @@ def get_frames_to_store(cap, mac_address):
         else:
             break
 
+def store_cameras_thread(cameraTuple):
+    cap = build_video_capture(cameraTuple[0])
+    get_frames_to_store(cap, cameraTuple[1])
+
+
 def store_cameras():
-    for camera_to_store in load_cameras():
-        gen_frames(camera_to_store[0], camera_to_store[1].mac_address, 'store')
-
-new_thread_to_save_videos_in_background = Thread(target=store_cameras)
-new_thread_to_save_videos_in_background.start()
-
-def get_frames_to_view(cap):
-    while True:
-        success, frame = cap.read()
-        if success:
-            try:
-                resized_frame = resize_img_from_matriz_size(get_cameras_matrix_size(), frame)
-                ret, buffer = cv2.imencode('.jpg', resized_frame)
-                resized_frame = buffer.tobytes()
-                yield (b'--frame\r\n'
-                    b'Content-Type: image/jpeg\r\n\r\n' + resized_frame + b'\r\n')
-            except Exception as e:
-                logging.exception(e)
-                print(e)
-                pass    
-        else:
-                pass
-def resize_img_from_matriz_size(camera_matrix_size, frame):
-    width = int(frame.shape[1] / camera_matrix_size)
-    height = int(frame.shape[0] / camera_matrix_size)
-    dim = (width, height)
-    return cv2.resize(frame, dim, interpolation = cv2.INTER_AREA)
+    camera_ips = load_cameras()
+    for camera in camera_ips:
+        ip = camera[0]
+        mac_address = camera[1].mac_address 
+        cameraTuple = (ip, mac_address)
+        new_thread_to_save_videos_in_background = Thread(target=store_cameras_thread, args=(cameraTuple,))
+        new_thread_to_save_videos_in_background.start()
     
-def convert_1d_to_2d(l, cols):
-    return [l[i:i + cols] for i in range(0, len(l), cols)]
+    #testando camera do notebook
+    cameraTuple = ('0', 'mac_address')
+    new_thread_to_save_videos_in_background = Thread(target=store_cameras_thread, args=(cameraTuple,))
+    new_thread_to_save_videos_in_background.start()
+    #testando camera do notebook
 
-def get_cameras_matrix_size():
-    cameras_quantity = len(camera_ips)
-    return math.ceil(math.sqrt(cameras_quantity))
+store_cameras()
+# recording camera locally
 
 @app.route('/')
 def index():
@@ -187,15 +192,19 @@ def cameras():
         camera_ips.append('0')
         #usando para testes
         
-        cameras_matrix = convert_1d_to_2d(camera_ips, get_cameras_matrix_size())
-        return render_template('cameras.html', cameras_matrix = cameras_matrix, camera_ips = camera_ips)
+        cameras_matrix = convert_1d_to_2d(camera_ips, get_cameras_matrix_size(camera_ips))
+        print(cameras_matrix)
+        
+        return render_template('cameras.html', cameras_matrix = cameras_matrix)
     return redirect(url_for('login_get'))
 
-@app.route('/video_feed/<string:ip>')
-def video_feed(ip):
+@app.route('/video_feed/<string:camera_ip>/<int:camera_matrix_size>')
+def video_feed(camera_ip, camera_matrix_size):
     if 'username' in session:
-        return Response(stream_with_context(gen_frames(ip,'view')), mimetype='multipart/x-mixed-replace; boundary=frame')
+        print(camera_matrix_size)
+        cap = build_video_capture(camera_ip)
+        return Response(stream_with_context(gen_frames_by_ip_to_view(cap, camera_matrix_size)), mimetype='multipart/x-mixed-replace; boundary=frame')
     return redirect(url_for('login_get'))
 
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0', threaded=True)
+    app.run(debug=False, host='0.0.0.0', threaded=True)
